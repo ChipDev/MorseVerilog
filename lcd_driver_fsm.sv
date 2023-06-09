c3edd6ac369d51f6a788c30dfb0fac25c755b659
stwt <kanssdev@gmail.com>
2023-06-09 12:47:37 -0700

    Update lcd_driver_fsm.sv

--- lcd_driver_fsm.sv
+++ lcd_driver_fsm.sv
@@ -36,12 +36,13 @@ module lcd_driver_fsm(
     logic[13:0] counter = 0;
     logic counter_enable = 0;
 
-    typedef enum { OFF, SETUP_0, WRITE_PULSE_WAIT, WRITE_P, DATA_WAIT, DATA_WRITE, SHUTDOWN} STATES;
+    typedef enum { OFF, SETUP_0, WRITE_PULSE_WAIT, DATA_WAIT, DATA_WRITE, SHUTDOWN, SHUTDOWN_2} STATES;
     STATES PS, NS;
-    STATES NEXT_COMMAND;
+    STATES COMMAND, NEXT_COMMAND;
 
     always_ff @(posedge CLK) begin
         PS <= NS;
+        COMMAND <= NEXT_COMMAND;
     end
 
     always_ff @(posedge CLK) begin
@@ -50,15 +51,14 @@ module lcd_driver_fsm(
         else counter <= counter+1;
     end
 
-    //assign STATE = {NEXT_COMMAND==DATA_WAIT, PS==WRITE_PULSE_WAIT, PS==DATA_WAIT};
-
     always_comb begin
         WRITING = 0;
         WAITING = 0;
         D = 8'b00000000;
-        NEXT_COMMAND = DATA_WAIT;
+        NEXT_COMMAND = COMMAND;
         case(PS) 
             OFF: begin
+                NEXT_COMMAND = DATA_WAIT;
                 RS = 0;
                 E = 0;
                 D = 8'b00000000;
@@ -81,8 +81,7 @@ module lcd_driver_fsm(
                 E = 0;
                 WAITING = 0;
                 if(counter == 13'b1111111111111) begin
-                    //NS = NEXT_COMMAND;
-                    NS = DATA_WAIT;
+                    NS = COMMAND;
                     E = 0;
                 end
                 else begin
@@ -121,7 +120,10 @@ module lcd_driver_fsm(
                 NS = DATA_WAIT;
                 RS = 0;
                 D = 8'b00000000; 
-                if(WRITE == 1) begin
+                if(!EN) begin
+                    NS = SHUTDOWN;
+                end
+                else if(WRITE == 1) begin
                     NS = DATA_WRITE;
                 end
                 else begin
@@ -149,52 +151,53 @@ module lcd_driver_fsm(
                 else begin
                     E = 0;
                 end
+            end       
+
+            SHUTDOWN: begin
+                WAITING = 1;
+                WRITING = 1;
+                counter_enable = 1;
+                NS = SHUTDOWN;
+                RS = 0;
+                D = 8'b00000001;
+                NEXT_COMMAND = SHUTDOWN_2;
+                if(counter == 13'b1111111111111) begin
+                    NS = WRITE_PULSE_WAIT;
+                    E = 0;
+                end
+                else if(counter > 2500) begin
+                    E = 0;
+                end
+                else if(counter > 500) begin
+                    E = 1;
+                end
+                else begin
+                    E = 0;
+                end
             end
 
-            // WRITE_P: begin
-            //     WRITING = 1;
-            //     counter_enable = 1;
-            //     NS = WRITE_P;
-            //     RS = 1;
-            //     D = 8'b01010000;
-            //     NEXT_COMMAND = DATA_WAIT;
-            //     if(counter == 13'b1111111111111) begin
-            //         NS = WRITE_PULSE_WAIT;
-            //         E = 0;
-            //     end
-            //     else if(counter > 2500) begin
-            //         E = 0;
-            //     end
-            //     else if(counter > 500) begin
-            //         E = 1;
-            //     end
-            //     else begin
-            //         E = 0;
-            //     end
-            // end
+            SHUTDOWN_2: begin
+                WRITING = 1;
+                counter_enable = 1;
+                NS = SHUTDOWN_2;
+                RS = 1;
+                D = 8'b00001110;
+                NEXT_COMMAND = DATA_WAIT;
+                if(counter == 13'b1111111111111) begin
+                    NS = WRITE_PULSE_WAIT;
+                    E = 0;
+                end
+                else if(counter > 2500) begin
+                    E = 0;
+                end
+                else if(counter > 500) begin
+                    E = 1;
+                end
+                else begin
+                    E = 0;
+                end
 
-            
-
-            // SHUTDOWN: begin
-            //     counter_enable = 1;
-            //     NS = SHUTDOWN;
-            //     RS = 0;
-            //     D = 8'b00000000;
-            //     NEXT_COMMAND = OFF;
-            //     if(counter == 13'b1111111111111) begin
-            //         NS = WRITE_PULSE_WAIT;
-            //         E = 0;
-            //     end
-            //     else if(counter > 2500) begin
-            //         E = 0;
-            //     end
-            //     else if(counter > 500) begin
-            //         E = 1;
-            //     end
-            //     else begin
-            //         E = 0;
-            //     end
-            // end
+            end
 
 
             default: begin
