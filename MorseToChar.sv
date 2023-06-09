e5ee24de773f8095ae6dde48ce29b31ecb983543
stwt <kanssdev@gmail.com>
2023-06-09 03:17:24 -0700

    Add files via upload

---
+++ MorseToChar.sv
@@ -0,0 +1,173 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Gus Flusser
+// 
+// Create Date: 06/08/2023 12:50:36 PM
+// Design Name:  
+// Module Name: MorseToChar
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MorseToChar(
+    input clk,
+    input recieve,
+    input [1:0] datain,
+    output logic valid,
+    output send,
+    output logic [7:0] bits,
+    output [7:0] lengthAnytime
+    );
+
+    logic[7:0] length = 8'b0;
+    logic[4:0] morse = 5'b0; 
+    logic sendData = 0;
+
+    assign lengthAnytime = length;
+
+    CLOCK_SYNC sendOnlyOne(.FAST(clk), .SLOW(sendData), .PULSE(send));
+
+    always_ff @(posedge recieve) begin
+
+        bits <= {3'b000, morse};
+        if(length == 8'b00000001) begin
+            case(morse)
+                0: begin
+                    bits <= 8'b01000101; // E
+                end
+                1: begin
+                    bits <= 8'b01010100; // T
+                end
+                default: begin
+                    bits <= 8'b0;
+                end
+            endcase
+        end
+        else if(length == 8'b00000010) begin
+            case(morse)
+                2'b00: begin
+                    bits <= 8'b01001001; // I
+                end
+                2'b01: begin
+                    bits <= 8'b01000001; // A
+                end
+                2'b10: begin
+                    bits <= 8'b01001110; // N
+                end
+                2'b11: begin
+                    bits <= 8'b01001101; // M
+                end
+                default: begin
+                    bits <= 8'b0;
+                end
+            endcase
+        end
+        else if(length == 8'b00000011) begin
+            case(morse)
+                3'b000: begin
+                    bits <= 8'b01010011; // S
+                end
+                3'b001: begin
+                    bits <= 8'b01010101; // U
+                end
+                3'b010: begin
+                    bits <= 8'b01010010; // R
+                end
+                3'b011: begin
+                    bits <= 8'b01010111; // W
+                end
+                3'b100: begin
+                    bits <= 8'b01000100; // D
+                end
+                3'b101: begin
+                    bits <= 8'b01001011; // K
+                end
+                3'b110: begin
+                    bits <= 8'b01000111; // G
+                end
+                3'b111: begin
+                    bits <= 8'b01001111; // O
+                end
+
+                default: begin
+                    bits <= 8'b0;
+                end
+            endcase
+        end
+        else if(length == 8'b00000100) begin
+            case(morse)
+                4'b0000: begin
+                    bits <= 8'b01001000; // H
+                end
+                4'b0001: begin
+                    bits <= 8'b01010110; // V
+                end
+                4'b0010: begin
+                    bits <= 8'b01000110; // F
+                end
+               
+                4'b0100: begin
+                    bits <= 8'b01001100; // L
+                end
+
+                4'b0110: begin
+                    bits <= 8'b01010000; // P
+                end
+                4'b0111: begin
+                    bits <= 8'b01001010; // J
+                end
+                4'b1000: begin
+                    bits <= 8'b01000010; // B
+                end
+                4'b1001: begin
+                    bits <= 8'b01011000; // X
+                end
+                4'b1010: begin
+                    bits <= 8'b01000011; // C
+                end
+                4'b1011: begin
+                    bits <= 8'b01011001; // Y
+                end
+                4'b1100: begin
+                    bits <= 8'b01011010; // Z 
+                end
+                4'b1101: begin
+                    bits <= 8'b01010001; // Q
+                end
+
+                default: begin
+                    bits <= 8'b0;
+                end
+            endcase
+        end
+
+        if(datain == 0) begin
+            sendData <= ~(length == 0);
+            morse <= 0;
+            length <= 0;
+            valid <= 0;
+        end
+        else begin
+            sendData <= 0;
+            length <= length + 1;
+            morse <= (morse << 1) + (datain - 1);
+            valid <= 1;
+        end
+    end
+
+
+
+
+
+endmodule
---
+++ lcd_driver_fsm.sv
@@ -0,0 +1,213 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Blake Stewart
+// 
+// Create Date: 06/06/2023 04:53:07 PM
+// Design Name: 
+// Module Name: lcd_driver_fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lcd_driver_fsm(
+    input CLK,
+    input EN,
+    input [7:0] CHAR,
+    input WRITE,
+    input SWITCH,
+    output logic RS,
+    output logic E,
+    output logic [7:0] D,
+    output logic WRITING,
+    output logic WAITING
+    );
+
+    logic[13:0] counter = 0;
+    logic counter_enable = 0;
+
+    typedef enum { OFF, SETUP_0, WRITE_PULSE_WAIT, WRITE_P, DATA_WAIT, DATA_WRITE, SHUTDOWN} STATES;
+    STATES PS, NS;
+    STATES NEXT_COMMAND;
+
+    always_ff @(posedge CLK) begin
+        PS <= NS;
+    end
+
+    always_ff @(posedge CLK) begin
+        if(!counter_enable) counter <= 0;
+        else if(counter == 13'b1111111111111) counter <= 13'b0;
+        else counter <= counter+1;
+    end
+
+    //assign STATE = {NEXT_COMMAND==DATA_WAIT, PS==WRITE_PULSE_WAIT, PS==DATA_WAIT};
+
+    always_comb begin
+        WRITING = 0;
+        WAITING = 0;
+        D = 8'b00000000;
+        NEXT_COMMAND = DATA_WAIT;
+        case(PS) 
+            OFF: begin
+                RS = 0;
+                E = 0;
+                D = 8'b00000000;
+                counter_enable = 0;
+
+                if(EN) begin 
+                    NS = SETUP_0;
+                end
+                else begin
+                    NS = OFF;
+                end
+
+            end
+
+            WRITE_PULSE_WAIT: begin
+                WAITING = 0;
+                D = 8'b00000000;
+                RS = 0;
+                counter_enable = 1;
+                E = 0;
+                WAITING = 0;
+                if(counter == 13'b1111111111111) begin
+                    //NS = NEXT_COMMAND;
+                    NS = DATA_WAIT;
+                    E = 0;
+                end
+                else begin
+                    NS = WRITE_PULSE_WAIT;
+                    E = 0;
+                end
+
+            end
+
+            SETUP_0: begin
+                WRITING = 0;
+                counter_enable = 1;
+                NS = SETUP_0;
+                RS = 0;
+                D = 8'b00001101;
+                NEXT_COMMAND = DATA_WAIT;
+                if(counter == 13'b1111111111111) begin
+                    NS = WRITE_PULSE_WAIT;
+                    E = 0;
+                end
+                else if(counter > 2500) begin
+                    E = 0;
+                end
+                else if(counter > 500) begin
+                    E = 1;
+                end
+                else begin
+                    E = 0;
+                end
+            end
+
+            DATA_WAIT: begin
+                E = 0;
+                WAITING = 1;
+                counter_enable = 0;
+                NS = DATA_WAIT;
+                RS = 0;
+                D = 8'b00000000; 
+                if(WRITE == 1) begin
+                    NS = DATA_WRITE;
+                end
+                else begin
+                    NS = DATA_WAIT;
+                end
+            end
+
+            DATA_WRITE: begin
+                WRITING = 1;
+                counter_enable = 1;
+                NS = DATA_WRITE;
+                RS = 1;
+                D = CHAR;
+                NEXT_COMMAND = DATA_WAIT;
+                if(counter == 13'b1111111111111) begin
+                    NS = WRITE_PULSE_WAIT;
+                    E = 0;
+                end
+                else if(counter > 2500) begin
+                    E = 0;
+                end
+                else if(counter > 500) begin
+                    E = 1;
+                end
+                else begin
+                    E = 0;
+                end
+            end
+
+            // WRITE_P: begin
+            //     WRITING = 1;
+            //     counter_enable = 1;
+            //     NS = WRITE_P;
+            //     RS = 1;
+            //     D = 8'b01010000;
+            //     NEXT_COMMAND = DATA_WAIT;
+            //     if(counter == 13'b1111111111111) begin
+            //         NS = WRITE_PULSE_WAIT;
+            //         E = 0;
+            //     end
+            //     else if(counter > 2500) begin
+            //         E = 0;
+            //     end
+            //     else if(counter > 500) begin
+            //         E = 1;
+            //     end
+            //     else begin
+            //         E = 0;
+            //     end
+            // end
+
+            
+
+            // SHUTDOWN: begin
+            //     counter_enable = 1;
+            //     NS = SHUTDOWN;
+            //     RS = 0;
+            //     D = 8'b00000000;
+            //     NEXT_COMMAND = OFF;
+            //     if(counter == 13'b1111111111111) begin
+            //         NS = WRITE_PULSE_WAIT;
+            //         E = 0;
+            //     end
+            //     else if(counter > 2500) begin
+            //         E = 0;
+            //     end
+            //     else if(counter > 500) begin
+            //         E = 1;
+            //     end
+            //     else begin
+            //         E = 0;
+            //     end
+            // end
+
+
+            default: begin
+                WRITING = 0;
+                counter_enable = 0;
+                NS = SHUTDOWN;
+                D = 8'b00000000;
+                RS = 0;
+                E = 0;
+            end
+
+        endcase
+    end
+
+
+endmodule
